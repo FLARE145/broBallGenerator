5fbc6b437e5033a1f2dd24e1b6d53da64f0b7891
thekikoman <kikoknowsyouall@gmail.com>
2024-08-08 22:07:42 -0700

    random phrases working

--- broBallGenerator.py
+++ broBallGenerator.py
@@ -1,15 +1,54 @@
 import json
 import random
+from PIL import Image
 
+#load image
+broBallPrime = Image.open("helloBro.jpg")
 
 #loading lists
-
 interjections = json.load(open("interjections.json"))
 adverbs = json.load(open("adverbs.json"))
 verbs = json.load(open("verbs.json"))
 adjectives = json.load(open("adjectives.json"))
+pronouns = json.load(open("pronouns.json"))
+contractions = json.load(open("contractions.json"))
 
-print(random.choice(interjections) + " bro")
+#50% chance a word will start uppercase
+def randomCase(text):
+    if random.randrange(2) > 0:
+        return text.capitalize()
+    else:
+        return text
 
-#def randomPhrase():
-    
+#picks a random word from a list and gives it random case
+def pick(list):
+    return randomCase(random.choice(list))
+
+#generates and prints random bro ball phrase
+def randomPhrase():
+    formula = random.randrange(10)
+    match formula:
+        case 0:
+            print(f"{pick(interjections)} Bro")
+        case 1:
+            print(f"{pick(adverbs)} Bro")
+        case 2:
+            print(f"{pick(verbs)} Bro")
+        case 3:
+            print(f"{randomCase('i')}'m {pick(adjectives)} Bro")
+        case 4:
+            print(f"{pick(contractions)} {randomCase('being')} {pick(adjectives)} Bro")
+        case 5:
+            print(f"{pick(verbs)} {pick(pronouns)} Bro")
+        case 6:
+            print(f"{pick(adverbs)} {pick(verbs)} {pick(pronouns)} Bro")
+        case 7:
+            print(f"{pick(verbs)} {pick(adverbs)} Bro")
+        case 8:
+            print(f"{pick(verbs)} {pick(pronouns)} {pick(adverbs)} Bro")
+        case 9:
+            print(f"{pick(contractions)} {pick(adjectives)} Bro")
+        case _:
+            print("uh oh bro")
+
+randomPhrase()
