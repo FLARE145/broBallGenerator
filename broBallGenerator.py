428378a37e45ab9546719c7a287b2dac02d4a820
thekikoman <kikoknowsyouall@gmail.com>
2024-08-10 00:21:55 -0700

    shows images

--- broBallGenerator.py
+++ broBallGenerator.py
@@ -1,20 +1,7 @@
 import json
 import random
-from PIL import Image
-from PIL import ImageFont, ImageDraw
-
-
-font = ImageFont.truetype("times.ttf", 15)
-
-#load image
-broBallPrime = Image.open("helloBro.jpg")
-area1 = (52, 18, 105, 32)
-area2 = (102, 88, 155, 102)
-square = broBallPrime.crop(area1)
-broBallPrime.paste(square, area2)
-ImageDraw.Draw(broBallPrime).text((10, 25), "world", font=font)
-broBallPrime.show()
-
+from PIL import Image, ImageFont, ImageDraw, ImageFilter, ImageOps
+ImageDraw.ImageDraw.font = ImageFont.truetype("times.ttf", 11.4)
 
 #loading lists
 interjections = json.load(open("interjections.json"))
@@ -26,7 +13,7 @@ contractions = json.load(open("contractions.json"))
 
 #50% chance a word will start uppercase
 def randomCase(text):
-    if random.randrange(2) > 0:
+    if random.randrange(4) > 0:
         return text.capitalize()
     else:
         return text
@@ -36,30 +23,68 @@ def pick(list):
     return randomCase(random.choice(list))
 
 #generates and prints random bro ball phrase
-def randomPhrase():
+def generatePhrase():
     formula = random.randrange(10)
     match formula:
         case 0:
-            print(f"{pick(interjections)} Bro")
+            return f"{pick(interjections)} Bro"
         case 1:
-            print(f"{pick(adverbs)} Bro")
+            return f"{pick(adverbs)} Bro"
         case 2:
-            print(f"{pick(verbs)} Bro")
+            return f"{pick(verbs)} Bro"
         case 3:
-            print(f"{randomCase('i')}'m {pick(adjectives)} Bro")
+            return f"{randomCase('i')}'m {pick(adjectives)} Bro"
         case 4:
-            print(f"{pick(contractions)} {randomCase('being')} {pick(adjectives)} Bro")
+            return f"{pick(contractions)} {randomCase('being')} {pick(adjectives)} Bro"
         case 5:
-            print(f"{pick(verbs)} {pick(pronouns)} Bro")
+            return f"{pick(verbs)} {pick(pronouns)} Bro"
         case 6:
-            print(f"{pick(adverbs)} {pick(verbs)} {pick(pronouns)} Bro")
+            return f"{pick(adverbs)} {pick(verbs)} {pick(pronouns)} Bro"
         case 7:
-            print(f"{pick(verbs)} {pick(adverbs)} Bro")
+            return f"{pick(verbs)} {pick(adverbs)} Bro"
         case 8:
-            print(f"{pick(verbs)} {pick(pronouns)} {pick(adverbs)} Bro")
+            return f"{pick(verbs)} {pick(pronouns)} {pick(adverbs)} Bro"
         case 9:
-            print(f"{pick(contractions)} {pick(adjectives)} Bro")
+            return f"{pick(contractions)} {pick(adjectives)} Bro"
         case _:
             print("uh oh bro")
 
-randomPhrase()
+def randomColor():
+    color = (random.choice(range(256)), random.choice(range(256)), random.choice(range(256)))
+    return color
+
+#generates bro ball image from phrase
+def generateImage(phrase):
+    print(phrase)
+    style = random.randrange(2)+1
+    base = f"bbBase{style}.jpg"
+    bubble = f"sb{style}.png"
+    with Image.open(base).convert("RGBA") as ball, Image.open(bubble) as speechBubble:
+        #creates new image as base for text
+        txt = Image.new("RGBA", ball.size, (255, 255, 255, 0))
+        d = ImageDraw.Draw(txt)
+        #manages where to place text depending on length
+        textCoord = (55, 17)
+        if d.textlength(phrase) > 110:
+            textCoord = (45, 17)
+            words = phrase.split()
+            print(phrase.split())
+            newPhrase = ""
+            for entry in words:
+                temp = newPhrase + f" {entry}"
+                if (d.textbbox((45,17), temp)[2] - d.textbbox((45,17), temp)[0]) > 85:
+                    newPhrase += "\n"
+                newPhrase += f" {entry}"
+            phrase = newPhrase
+        elif d.textlength(phrase) > 70:
+            textCoord = (39, 33)
+        #places text on new image and combines it onto the bb image
+        d.text(textCoord, phrase, fill=(0, 0, 0, 255))
+        bluredText = txt.filter(ImageFilter.GaussianBlur(.5/style))
+        coloredBall = ImageOps.colorize(ball.convert("L"), randomColor(), "white")
+        withBubble = Image.alpha_composite(coloredBall.convert("RGBA"), speechBubble)
+        combine = Image.alpha_composite(withBubble, bluredText)
+        out = combine.resize((1280, 1200))
+        out.show()
+
+generateImage(generatePhrase())
